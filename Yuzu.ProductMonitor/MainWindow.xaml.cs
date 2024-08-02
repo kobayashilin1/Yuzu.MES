2939d6c7b5366f52f101c285db3422480139aa1b
QIULIN CHEN <yuzu1005@qq.com>
2024-08-02 14:14:53 +0800

    创建主页第二行的用户控件，并绑定VM

--- Yuzu.ProductMonitor/MainWindow.xaml.cs
+++ Yuzu.ProductMonitor/MainWindow.xaml.cs
@@ -8,17 +8,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Yuzu.ProductMonitor.ViewModels;
 
 namespace Yuzu.ProductMonitor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowVM();
         }
     }
 }
---
+++ Yuzu.ProductMonitor/ViewModels/MainWindowVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Yuzu.ProductMonitor.UserControls;
+
+namespace Yuzu.ProductMonitor.ViewModels
+{
+    /// <summary>
+    /// 主界面的视图模型（ViewModel）
+    /// </summary>
+    public class MainWindowVM : ObservableObject
+    {
+        // 监视用户控件：因为以后会定义别的控件，所以类型统一使用 UserControl
+        private UserControl? monitorUserControl;
+        public UserControl? MonitorUserControl
+        {
+            get
+            {
+                // 若为空，则实例化一个 UserContro 控件
+                return monitorUserControl ?? new MonitorUserControl();
+            }
+            set
+            {
+                monitorUserControl = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
