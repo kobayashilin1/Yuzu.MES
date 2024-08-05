ac9ac434b80e871e4150a28c4597f2c592937df4
QIULIN CHEN <yuzu1005@qq.com>
2024-08-05 17:53:34 +0800

    添加按钮自定义命令

---
+++ Yuzu.ProductMonitor/Commands/Command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Yuzu.ProductMonitor.Commands
+{
+    public class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        // 定义委托
+        private Action action;
+        public Action Action { get { return action; } }
+
+        // 构造方法
+        public Command(Action action)
+        {
+            this.action = action;
+        }
+
+        // 是否可执行
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (action != null) 
+            { 
+                this.action();
+            }
+        }
+    }
+}
--- Yuzu.ProductMonitor/MainWindow.xaml.cs
+++ Yuzu.ProductMonitor/MainWindow.xaml.cs
@@ -8,16 +8,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Yuzu.ProductMonitor.UserControls;
 using Yuzu.ProductMonitor.ViewModels;
+using Yuzu.ProductMonitor.Commands;
 
 namespace Yuzu.ProductMonitor
 {
     public partial class MainWindow : Window
     {
+        // 视图
+        private MainWindowVM mainWindow = new MainWindowVM();
+
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowVM();
+            this.DataContext = mainWindow;
+        }
+
+        // 显示车间详情页
+        private void ShowWorkShopDetailUC()
+        {
+            WorkShopDetailUserControl workShopDetailUC = new WorkShopDetailUserControl();
+
+            // 更改视图模型
+            mainWindow.MonitorUserControl = workShopDetailUC;
+        }
+
+        // 展示详情命令
+        public Command ShowDetailCmd
+        {
+            get
+            {
+                return new Command(ShowWorkShopDetailUC);
+            }
         }
     }
 }
